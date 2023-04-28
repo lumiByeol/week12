e600ddac3e91bde72a1cc953bd3f71ee35b98245
lumiByeol <julie.karakulova@gmail.com>
2023-04-28 21:52:40 +0300

    attempt two

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String s = "";
+
+        try (FileReader fileReader = new FileReader("text.txt"); FileWriter fileWriter = new FileWriter("text2.txt")) {
+            int inChar = fileReader.read();
+            while (inChar != -1) {
+                s += (char) inChar;
+                inChar = fileReader.read();
+            }
+            fileWriter.write(s);
+        }
+        catch(IOException e) {
+            System.err.println(e);
+        }
     }
 }
